975683ab4f42cb1c8af4c19ac583c1e13ffa4274
sandhiya2203 <107338809+sandhiya2203@users.noreply.github.com>
2026-07-13 15:47:26 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import time
+from datetime import datetime
 
 app = FastAPI()
 
@@ -17,35 +18,45 @@ patient_data = {
     "heart_rate": 0,
     "spo2": 0,
     "systolic_bp": 0,
-    "diastolic_bp": 0
+    "diastolic_bp": 0,
+    "last_update": "No Data"
 }
 
 
 # Store last communication time
-last_update_time = time.time()
+last_update_seconds = time.time()
 
 
 @app.post("/update")
 def update_patient(data: dict):
 
-    global patient_data, last_update_time
+    global patient_data, last_update_seconds
 
+    # Receive data from consumer
     patient_data = data
 
-    # update heartbeat time
-    last_update_time = time.time()
+    # Update heartbeat time
+    last_update_seconds = time.time()
 
-    return {"message": "Updated"}
+    # Store last received time
+    patient_data["last_update"] = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    return {
+        "message": "Updated"
+    }
 
 
 
 @app.get("/patient")
 def get_patient():
 
-    global patient_data, last_update_time
+    global patient_data, last_update_seconds
 
-    # If no data received for 5 seconds
-    if time.time() - last_update_time > 5:
+
+    # No data received for 5 seconds
+    if time.time() - last_update_seconds > 5:
 
         patient_data["status"] = "Disconnected"
 
